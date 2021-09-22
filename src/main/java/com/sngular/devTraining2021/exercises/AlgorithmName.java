a3310a511ced7b7a2290118d05fa980437b53fc2
Yael Estrada <yael_ex2.a97@hotmail.com>
2021-09-22 12:45:01 -0500

    Ejercicio 02

--- src/main/java/com/sngular/devTraining2021/exercises/AlgorithmName.java
+++ src/main/java/com/sngular/devTraining2021/exercises/AlgorithmName.java
@@ -2,7 +2,21 @@ package com.sngular.devTrining2021.exercises;
 
 public class AlgorithmName {
     public static String accum(String str) {
-        //Implements the function with the algorithm
-        return null;
+   	String s="";
+	int val;
+	char letra;
+	for(int i=0;i<str.length();i++){
+		val=(int)str.charAt(i);
+		letra=val<91&&val>64? (char)(val+32):str.charAt(i);
+		for(int j=0;j<=i;j++){
+			if(j==0)
+				s+=(char)(letra-32);
+			else
+				s+=letra;
+		}
+		if(i!=str.length()-1)
+			s+="-";
+	}
+	return s;
     }
 }
--- src/test/java/com/sngular/devTrianing2021/exercises/TestsCasesAlgorithmName.java
+++ src/test/java/com/sngular/devTrianing2021/exercises/TestsCasesAlgorithmName.java
@@ -1,4 +1,4 @@
-package com.sngular.devTrining2021.test.exercises;
+package com.sngular.devTrining2021.exercises;
 
 import org.junit.Test;
 import static org.junit.Assert.*;
@@ -6,9 +6,9 @@ import static org.junit.Assert.*;
 public class TestsCasesAlgorithmName {
     @Test
     public void test() {
-        assertEquals("Test value ZpglnRxqenU","Z-Pp-Ggg-Llll-Nnnnn-Rrrrrr-Xxxxxxx-Qqqqqqqq-Eeeeeeeee-Nnnnnnnnnn-Uuuuuuuuuuu",AlgorithmName.accum("ZpglnRxqenU"),);
+        assertEquals("Test value ZpglnRxqenU","Z-Pp-Ggg-Llll-Nnnnn-Rrrrrr-Xxxxxxx-Qqqqqqqq-Eeeeeeeee-Nnnnnnnnnn-Uuuuuuuuuuu",AlgorithmName.accum("ZpglnRxqenU"));
         assertEquals("Test value NyffsGeyylB","N-Yy-Fff-Ffff-Sssss-Gggggg-Eeeeeee-Yyyyyyyy-Yyyyyyyyy-Llllllllll-Bbbbbbbbbbb",AlgorithmName.accum("NyffsGeyylB"));
-        assertEquals("Test value MjtkuBovqrU","M-Jj-Ttt-Kkkk-Uuuuu-Bbbbbb-Ooooooo-Vvvvvvvv-Qqqqqqqqq-Rrrrrrrrrr-Uuuuuuuuuuu",AlgorithmName.accum("MjtkuBovqrU"),);
+        assertEquals("Test value MjtkuBovqrU","M-Jj-Ttt-Kkkk-Uuuuu-Bbbbbb-Ooooooo-Vvvvvvvv-Qqqqqqqqq-Rrrrrrrrrr-Uuuuuuuuuuu",AlgorithmName.accum("MjtkuBovqrU"));
         assertEquals("Test value EvidjUnokmM","E-Vv-Iii-Dddd-Jjjjj-Uuuuuu-Nnnnnnn-Oooooooo-Kkkkkkkkk-Mmmmmmmmmm-Mmmmmmmmmmm",AlgorithmName.accum("EvidjUnokmM"));
         assertEquals("Test value HbideVbxncC","H-Bb-Iii-Dddd-Eeeee-Vvvvvv-Bbbbbbb-Xxxxxxxx-Nnnnnnnnn-Cccccccccc-Ccccccccccc",AlgorithmName.accum("HbideVbxncC"));
     }
