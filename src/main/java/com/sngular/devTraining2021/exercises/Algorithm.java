ccbde72a94a1cec15486b16814bbe64dcc119875
Yael Estrada <yael_ex2.a97@hotmail.com>
2021-09-22 15:36:28 -0500

    Ejercicio 06

--- src/main/java/com/sngular/devTraining2021/exercises/Algorithm.java
+++ src/main/java/com/sngular/devTraining2021/exercises/Algorithm.java
@@ -2,6 +2,20 @@ package com.sngular.devTrining2021.exercises;
 
 public class Algorithm {
     public static String startOz(String str) {
-     // implement algorith
+    	String s="";
+	int indo=str.indexOf("o");
+	int indz=str.indexOf("z");
+	int lastz=str.lastIndexOf("z");
+	if(indz==-1&&indo==-1) return "";
+	if(indo!=-1){
+		s+="o";
+		if(indz>indo)
+			s+="z";	
+	}
+	else{
+		if(indz!=lastz) s+="zz";
+		else s+="z";
+	}
+	return s;
     }
 }
