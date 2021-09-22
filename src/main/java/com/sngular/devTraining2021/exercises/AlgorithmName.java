4400bd3758cfb4a4143d6be0d3ffdf7e4e9c5412
Yael Estrada <yael_ex2.a97@hotmail.com>
2021-09-22 13:07:35 -0500

    Ejercicio 03

--- src/main/java/com/sngular/devTraining2021/exercises/AlgorithmName.java
+++ src/main/java/com/sngular/devTraining2021/exercises/AlgorithmName.java
@@ -2,7 +2,14 @@ package com.sngular.devTrining2021.exercises;
 
 public class AlgorithmName {
     public static boolean isIsogram(String str){
-        //Implements the function with the algorithm
-        return false;
+	str=str.toLowerCase();
+	int[] alphabet=new int[27];
+	for(int i=0;i<27;i++) alphabet[i]=0;
+	for(int i=0;i<str.length();i++){
+		alphabet[str.charAt(i)-97]++;
+		if(alphabet[str.charAt(i)-97]>1)
+			return false;
+	}
+	return true;
     }
 }
