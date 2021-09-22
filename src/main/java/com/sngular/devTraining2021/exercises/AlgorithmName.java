1487ee0822b4f7e36f92f1dccb5113c02ea1276a
Yael Estrada <yael_ex2.a97@hotmail.com>
2021-09-22 14:30:27 -0500

    Ejercicio 05

--- src/main/java/com/sngular/devTraining2021/exercises/AlgorithmName.java
+++ src/main/java/com/sngular/devTraining2021/exercises/AlgorithmName.java
@@ -2,6 +2,14 @@ package com.sngular.devTrining2021.exercises;
 
 public class AlgorithmName {
     public static int[] minMax(int[] arr) {
-        // Your awesome code here
+        int mini=arr[0],maxi=arr[0];
+	int[] ans=new int[2];
+	for(int i=0;i<arr.length;i++){
+		mini=arr[i]<mini?arr[i]:mini;
+		maxi=arr[i]>maxi?arr[i]:maxi;
+	}
+	ans[0]=mini;
+	ans[1]=maxi;
+	return ans;
     }
 }
