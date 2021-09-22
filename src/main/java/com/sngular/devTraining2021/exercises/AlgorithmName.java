1dcb232c7e185d8ef722f76867867235162b594d
Yael Estrada <yael_ex2.a97@hotmail.com>
2021-09-22 11:12:25 -0500

    Ejercicio 01

--- src/main/java/com/sngular/devTraining2021/exercises/AlgorithmName.java
+++ src/main/java/com/sngular/devTraining2021/exercises/AlgorithmName.java
@@ -1,8 +1,15 @@
 package com.sngular.devTrining2021.exercises;
-
+import java.util.Arrays;
 public class AlgorithmName {
     public static int orderNumber(int number){
-        //Implements the function with the algorithm
-        return 0;
+    	String s=""+number;
+	char temp[]=s.toCharArray();
+	Arrays.sort(temp);
+	for(int i=0;i<s.length()-1-i;i++){
+		char aux=temp[i];
+		temp[i]=temp[s.length()-1-i];
+		temp[s.length()-1-i]=aux;
+    	}
+	return Integer.parseInt(new String(temp));
     }
 }
